035273de306f0766b0893d8a98c8f78bc8f568ce
Eutarides <javifarmacastillo@gmail.com>
2024-03-15 15:01:05 +0100

    React primeros pasos

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Form from "./components/Form";
+import List from "./components/List";
+
+const App = () => {
+	return (
+		<div className="container content-center">
+			<div className="row mt-3">
+				<Form></Form>
+				<List></List>
+			</div>
+		</div>
+	)
+}
+
+export default App;
---
+++ vite-project/src/components/List.jsx
@@ -0,0 +1,13 @@
+const List = () => {
+	return (
+		<div className="col-md-7">
+			<div className="card">
+				<div className="card-header">Lista de estudiantes</div>
+				<div className="card-body">
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default List
